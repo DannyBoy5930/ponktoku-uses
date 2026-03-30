2f99cce4592100dee8ffa6f0542347858c95b483
Danny Bao <88077806+ponktoku@users.noreply.github.com>
2026-03-30 17:32:25 +1100

    fix: Change z import from astro:content to astro/zod

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,6 +1,7 @@
 // 1. Import utilities from `astro:content`
-import { z, defineCollection } from "astro:content";
+import { defineCollection } from "astro:content";
 import { glob } from "astro/loaders";
+import { z } from "astro/zod";
 
 // 2. Define your collection(s)
 const postsCollection = defineCollection({
